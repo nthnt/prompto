f69afce7df74bb6c58340fd8be3c61c8dfed87ab
Marcus Tuen Muk <tmmarcus73@gmail.com>
2022-09-17 23:35:04 -0400

    added fast api to set and get value of the DB

--- cockroachDB/main.py
+++ cockroachDB/main.py
@@ -1,17 +1,41 @@
 from fastapi import FastAPI
 from promptoDB import *
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Cat(BaseModel):
+    category: str
+    id: int
+    phrase: str
+
+class Model(BaseModel):
+    id: int
+    userPrompt: str
+    phrase: str
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
-@app.post("/db")
+@app.get("/db")
 async def db():
     table = Table()
-    table.createTable("models", "userPrompt", "STRING NOT NULL")
-    table.addColumn("models","phrase", "STRING NOT NULL")
+    table.createTable("hate", "phrase", "STRING NOT NULL")
     
+    return {"message": "Success"}
+
+@app.post("/setValue/")
+async def setValue(cat: Cat):
+    table = Table()
+    table.setValue(cat.category, ["ID", "phrase"], cat.id, [cat.phrase])
+
+    return {"message": "Success"}
+
+@app.get("/getValue")
+async def getValue(cat: Cat):
+    table = Table()
+    table.getValue(cat.category)
+
     return {"message": "Success"}
